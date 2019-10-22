7d8f09990359e59f86095854bb26dcd541af4cec
Aleksandr Vedrov <aleksandr.v@miro.com>
2019-10-22 13:03:11 +0500

    Commit

--- src/main/java/com/coveros/demo/helloworld/TestClass.java
+++ src/main/java/com/coveros/demo/helloworld/TestClass.java
@@ -2,15 +2,14 @@ package com.coveros.demo.helloworld;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.JUnitCore;
 
 public class TestClass {
 
     @Test
     public void testConcatenate() {
         HelloWorld myUnit = new HelloWorld();
-
         String result = myUnit.concatenate("one", "two");
-
         assertEquals("onetwo", result);
     }
 }
